48d167d19e8285644a880ebf80675b21fcaec1a9
Bhetmun <147073486+Bhetmun@users.noreply.github.com>
2023-12-15 13:58:56 +0000

    Add files via upload

---
+++ DMIS AI/AIWork/2687691b_W1.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Week 1 : Getting started with Anaconda, Jupyter Notebook and Python
+# 
+# Exercise to familiarise myself with Jupyter Notebook and its relationship to Python.
+
+# #### Reasons for joining the course
+# I wanted to learn more about AI and try it out 
+# 
+# #### Prior Experience 
+# I have no experience with coding AI but I have some basic coding experience with html and css
+# 
+# #### What I expect to learn from the course
+# 
+# -How to code using Python language 
+# -Why AI will continue to develop and become more mainstream 
+# -How I can use AI in my own life 
+
+# In[ ]:
+
+
+print ("Hello world!")
+
+
+# In[ ]:
+
+
+message = "Bomba"
+
+print (message)
+
+
+# In[ ]:
+
+
+message = "beans"
+
+print (message + message)
+
+
+# In[ ]:
+
+
+message = "beans"
+
+print (message*3)
+
+
+# In[ ]:
+
+
+message = "beans"
+
+print (message[0])
+
+
+# In[ ]:
+
+
+message = "beans"
+
+print (message[1])
+
+
+# In[ ]:
+
+
+print (message)
+
+
+# In[ ]:
+
+
+from IPython.display import *
+
+
+# In[ ]:
+
+
+YouTubeVideo ("05PKG_pWsVY")
+
+
+# In[ ]:
+
+
+
+
